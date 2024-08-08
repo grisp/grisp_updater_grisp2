d34e223aa0459fb2cd34f95b0202f1b51b5a1b56
Sebastien Merle <s.merle@gmail.com>
2024-08-08 09:14:52 +0200

    Adapte to grisp_updater API changes, add hardware emulation

--- src/grisp_updater_grisp2.erl
+++ src/grisp_updater_grisp2.erl
@@ -16,8 +16,7 @@
 % Behaviour grisp_updater_source callbacks
 -export([system_init/1]).
 -export([system_get_global_target/1]).
--export([system_get_active/1]).
-% -export([system_get_updatable/1]).
+-export([system_get_systems/1]).
 -export([system_prepare_update/2]).
 -export([system_prepare_target/4]).
 -export([system_set_updated/2]).
@@ -32,9 +31,11 @@
 %--- Records -------------------------------------------------------------------
 
 -record(sys_state, {
-    current :: grisp_updater_system:system_id() | removable,
-    active :: non_neg_integer(),
-    update :: non_neg_integer()
+    target :: target(),
+    boot :: grisp_updater_system:system_id() | removable,
+    valid :: grisp_updater_system:system_id(),
+    update :: grisp_updater_system:system_id(),
+    next :: grisp_updater_system:system_id()
 }).
 
 -record(http_state, {
@@ -47,34 +48,31 @@
 
 system_init(_Opts) ->
     ?LOG_INFO("Initializing GRiSP2 update system interface", []),
-    #{bootstate := #{active_system := Active, update_system := Update}} =
-        grisp_barebox:get_all(),
-    Current = current_system(),
-    {ok, #sys_state{
-        current = Current,
-        active = Active,
-        update = Update
-    }}.
+    {ok, update_state(#sys_state{
+        target = init_target(),
+        boot = boot_system()
+    })}.
 
-system_get_global_target(_State) ->
-    #target{device = <<"/dev/mmcsd-0">>, offset = 0, size = undefined}.
+system_get_global_target(#sys_state{target = Target}) ->
+    Target.
 
-system_get_active(#sys_state{current = Curr, active = Act}) ->
-    {Curr, Curr =:= Act}.
+system_get_systems(#sys_state{boot = Boot, next = Next, valid = Valid}) ->
+    {Boot, Valid, Next}.
 
-% system_get_updatable(#state{}) ->
-%     {error, not_updatable}.
-
-system_prepare_update(#sys_state{current = 0} = State, 1) ->
-    grisp_rtems:unmount(<<"/media/mmcsd-0-1">>),
+system_prepare_update(#sys_state{valid = SysId}, SysId) ->
+    {error, cannot_update_validated_system};
+system_prepare_update(#sys_state{boot = SysId}, SysId) ->
+    {error, cannot_update_boot_system};
+system_prepare_update(#sys_state{boot = Boot, valid = 0} = State, 1)
+  when Boot =:= removable; Boot =:= 0 ->
+    unmount_system(1),
     {ok, State};
-system_prepare_update(#sys_state{current = 1} = State, 0) ->
-    grisp_rtems:unmount(<<"/media/mmcsd-0-0">>),
+system_prepare_update(#sys_state{boot = Boot, valid = 1} = State, 0)
+  when Boot =:= removable; Boot =:= 1 ->
+    unmount_system(0),
     {ok, State};
-system_prepare_update(#sys_state{current = SysId}, SysId) ->
-    {error, cannot_update_running_system};
-system_prepare_update(_State, SysId) ->
-    {error, {invalid_update_system, SysId}}.
+system_prepare_update(#sys_state{boot = Boot, valid = Valid}, SysId) ->
+    {error, {unexpected_update_state, {Boot, Valid, SysId}}}.
 
 system_prepare_target(_State, _SysId,
                       #target{offset = SysOffset, size = SysSize} = SysTarget,
@@ -85,26 +83,19 @@ system_prepare_target(_State, _SysId,
         size = SysSize - ObjOffset
     }}.
 
-system_set_updated(#sys_state{current = 0} = State, 1) ->
-    grisp_barebox:set([bootstate, update_system], 1),
-    grisp_barebox:set([bootstate, update_boot_count], 1),
-    grisp_barebox:commit(),
-    {ok, State};
-system_set_updated(#sys_state{current = 1} = State, 0) ->
-    grisp_barebox:set([bootstate, update_system], 0),
-    grisp_barebox:set([bootstate, update_boot_count], 1),
-    grisp_barebox:commit(),
-    {ok, State}.
+system_set_updated(#sys_state{boot = Boot, valid = 0} = State, 1)
+  when Boot =:= removable; Boot =:= 0 ->
+    {ok, mark_updated(State, 1)};
+system_set_updated(#sys_state{boot = Boot, valid = 1} = State, 0)
+  when Boot =:= removable; Boot =:= 1 ->
+    {ok, mark_updated(State, 0)};
+system_set_updated(#sys_state{boot = Boot, valid = Valid}, SysId) ->
+    {error, {unexpected_update_state, {Boot, Valid, SysId}}}.
 
-system_validate(#sys_state{current = SysId, active = SysId} = State) ->
-    {ok, State};
-system_validate(#sys_state{current = SysId, update = SysId} = State) ->
-    grisp_barebox:set([bootstate, active_system], SysId),
-    grisp_barebox:set([bootstate, update_boot_count], 0),
-    grisp_barebox:commit(),
-    {ok, State#sys_state{active = SysId}};
-system_validate(#sys_state{current = removable}) ->
-    {error, current_system_removable}.
+system_validate(#sys_state{boot = SysId, update = SysId} = State) ->
+    {ok, validate_update(State, SysId)};
+system_validate(#sys_state{update = UpdatSysId}) ->
+    {error, {validate_from_unbooted, UpdatSysId}}.
 
 system_terminate(_State, _Reason) ->
     ?LOG_INFO("Terminating GRiSP update system interface", []),
@@ -123,7 +114,7 @@ http_connection_options(State, Url) ->
             Port = maps:get(port, Parts, 443),
             GunOpts = #{
                 transport => tls,
-                transport_opts => grisp_cryptoauth_tls:options(Hostname)
+                tls_opts => grisp_cryptoauth_tls:options(Hostname)
             },
             {ok, Hostname, Port, GunOpts, State};
         #{scheme := <<"http">>, host := Host} = Parts ->
@@ -137,12 +128,75 @@ http_connection_options(State, Url) ->
 
 %--- Internal Functions --------------------------------------------------------
 
-current_system() ->
+-ifdef(EMULATE_HARDWARE).
+
+-define(DEVICE_FILE, <<"dummy.img">>).
+-define(DEVICE_SIZE, ((4 + 256 + 256) * (1024 * 1024))).
+
+init_target() ->
+    DeviceFile = ?DEVICE_FILE,
+    DeviceSize = ?DEVICE_SIZE,
+    case file:open(?DEVICE_FILE, [raw, write, read]) of
+        {error, Reason} -> error(Reason);
+        {ok, File} ->
+            case file:pread(File, DeviceSize - 1, 1) of
+                {error, Reason} -> error(Reason);
+                {ok, [_]} ->
+                    ok = file:close(File);
+                eof ->
+                    ok = file:pwrite(File, DeviceSize - 1, <<0>>),
+                    ok = file:close(File)
+            end
+    end,
+    #target{device = DeviceFile, offset = 0,
+            size = DeviceSize, total = DeviceSize}.
+
+% boot_system() -> removable.
+boot_system() -> 0.
+
+unmount_system(_SysId) -> ok.
+
+-else. % EMULATE_HARDWARE is undefined
+
+init_target() ->
+    #target{device = <<"/dev/mmcsd-0">>, offset = 0}.
+
+boot_system() ->
     % TODO: Uses current working directory to figure out the current
     % booted system, should be changed to use the device tree.
-    case {?IS_EMULATED, file:get_cwd()} of
-        {true, _} -> 0;
-        {false, {ok, "/media/mmcsd-0-0"}} -> 0;
-        {false, {ok, "/media/mmcsd-0-1"}} -> 1;
-        {false, {ok, "/media/mmcsd-1-" ++ _}} -> removable
+    case file:get_cwd() of
+        {ok, "/media/mmcsd-0-0"} -> 0;
+        {ok, "/media/mmcsd-0-1"} -> 1;
+        {ok, "/media/mmcsd-1-" ++ _} -> removable
     end.
+
+unmount_system(0) -> grisp_rtems:unmount(<<"/media/mmcsd-0-0">>);
+unmount_system(1) -> grisp_rtems:unmount(<<"/media/mmcsd-0-1">>).
+
+-endif.
+
+update_state(State) ->
+    ActiveSysId = grisp_barebox:get([bootstate, active_system]),
+    UpdateSysId = grisp_barebox:get([bootstate, update_system]),
+    BootCount = grisp_barebox:get([bootstate, update_boot_count]),
+    NextSysId = case {ActiveSysId, UpdateSysId, BootCount} of
+        {_ActiveSysId, UpdateSysId, Count} when Count > 0 -> UpdateSysId;
+        {ActiveSysId, _UpdateSysId, 0} -> ActiveSysId
+    end,
+    State#sys_state{
+        next = NextSysId,
+        update = UpdateSysId,
+        valid = ActiveSysId
+    }.
+
+mark_updated(State, SysId) ->
+    grisp_barebox:set([bootstate, update_system], SysId),
+    grisp_barebox:set([bootstate, update_boot_count], 1),
+    grisp_barebox:commit(),
+    update_state(State).
+
+validate_update(State, SysId) ->
+    grisp_barebox:set([bootstate, active_system], SysId),
+    grisp_barebox:set([bootstate, update_boot_count], 0),
+    grisp_barebox:commit(),
+    update_state(State).
